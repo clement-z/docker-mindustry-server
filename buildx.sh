128e5c86d851173d2e095d15f8044e1e89210051
Clément Zrounba <6691770+clement-z@users.noreply.github.com>
2020-12-09 17:57:12 +0100

    update buildx tag (same logic for tag/mindustry release variables as build-and-push.sh)

--- buildx.sh
+++ buildx.sh
@@ -1,9 +1,17 @@
 #! /bin/bash -x
 
+#TAG="v121"
+#TARGET_RELEASE="v121"
+
+# Set defaults
+TAG=${TAG:-"latest"}
+TARGET_RELEASE=${TARGET_RELEASE:-"latest"}
+
+########################
 function init() {
-	local release_tag="v0.4.2"
+	local buildx_release_tag="v0.4.2"
 	mkdir -p ~/.docker/cli-plugins
-	curl -sSL "https://github.com/docker/buildx/releases/download/$release_tag/buildx-$release_tag.linux-amd64" -o ~/.docker/cli-plugins/docker-buildx
+	curl -sSL "https://github.com/docker/buildx/releases/download/$release_tag/buildx-$buildx_release_tag.linux-amd64" -o ~/.docker/cli-plugins/docker-buildx
 	chmod +x ~/.docker/cli-plugins/docker-buildx
 }
 
@@ -17,9 +25,9 @@ function build() {
 	local full_image_name=$1
 	local platforms=$2
 	docker buildx build -t $full_image_name \
+		--build-arg "MINDUSTRY_RELEASE"="$TARGET_RELEASE" \
 		--push --platform $platforms \
 		--label "build"="`git rev-parse HEAD`" .
-
 }
 
 docker buildx &>/dev/null || \
@@ -29,4 +37,4 @@ docker buildx ls | grep -e '^mybuilder' &>/dev/null || \
 	create
 
 cd docker
-build clementz/mindustry-server:latest linux/arm,linux/arm64,linux/amd64
+build clementz/mindustry-server:$TAG linux/arm,linux/arm64,linux/amd64
